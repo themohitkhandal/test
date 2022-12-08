a12f71fca3452f7c51aeb245ff8c1290f0f8a8a3
Mohit Khandal <mohit1319khandal@gmail.com>
2022-12-08 10:19:58 +0530

    first commit

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << i << ". " << "Mohit" << endl;
+    }
+    
+    return 0;
+}
